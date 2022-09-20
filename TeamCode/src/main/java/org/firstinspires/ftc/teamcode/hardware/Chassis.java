c8a7252e8ce1ab651328b2bbb64eec6f935ef795
Eblob123 <ethanh74324@gmail.com>
2022-09-20 18:10:37 -0400

    rr files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Chassis.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@Config
+public class Chassis {
+    DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
+
+    Gamepad gamepad1;
+    LinearOpMode opMode;
+    IMU imu;
+
+    double powerScalar = 0.6;
+
+    //Teleop Constructor
+    public Chassis(HardwareMap hardwareMap, IMU imu, Gamepad gamepad1){
+     this.gamepad1 = gamepad1;
+     this.imu = imu;
+
+     frontLeftMotor = hardwareMap.get(DcMotor.class, "FLM");
+     backLeftMotor = hardwareMap.get(DcMotor.class, "BLM");
+     frontRightMotor = hardwareMap.get(DcMotor.class, "FRM");
+     backRightMotor = hardwareMap.get(DcMotor.class, "BRM");
+
+     frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+     backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+     frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+     backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+     frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+     backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void FieldCentricDrive(){
+        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double x = gamepad1.left_stick_x; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
+
+        double botHeading = -imu.getHeadingFirstAngle();
+
+        double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+        double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+        frontLeftMotor.setPower(frontLeftPower * powerScalar);
+        backLeftMotor.setPower(backLeftPower * powerScalar);
+        frontRightMotor.setPower(frontRightPower * powerScalar);
+        backRightMotor.setPower(backRightPower * powerScalar);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TempFieldCentricDrive.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class TempFieldCentricDrive extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("FLM");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("BLM");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("FRM");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("BRM");
+
+        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            double botHeading = -imu.getAngularOrientation().firstAngle;
+
+            double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+            double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            motorFrontLeft.setPower(frontLeftPower * 0.6);
+            motorBackLeft.setPower(backLeftPower * 0.6);
+            motorFrontRight.setPower(frontRightPower * 0.6);
+            motorBackRight.setPower(backRightPower * 0.6);
+        }
+    }
+}
